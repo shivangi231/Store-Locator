c175b7b0b875c46ba63f882f0c0f7598f69d05f7
geraltofrivia <devl.priyansh@gmail.com>
2015-03-28 13:33:56 +0530

    v0.0 A basic web app with working 	- Jinja2 Writing (not rendering) 	- Basic Cookies (unhashed)
    Todo in next version
    	- Render a basic HTML page
    	- Test username cookies

---
+++ main.py
@@ -0,0 +1,37 @@
+import webapp2
+import os
+import jinja2
+import cgi
+
+#Setup templating engine
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.write(*a,**kw)
+
+	def render_Str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_Str(template, **kw))
+
+
+#Basic
+class MainPage(Handler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+		visits = self.request.cookies.get('visits','0')
+		if visits.isdigit():
+			visits = int(visits) + 1
+		else:
+			visits = 0
+		self.write("You have been here %s times! \n\n" % visits)
+		self.response.headers.add_header('Set-cookie', 'visits = %s' % visits)
+		#self.write(self.response)
+
+application = webapp2.WSGIApplication([
+									('/',MainPage)
+									], debug=True)
