a47a490ffcfe4efe9884dd64f7188a681c2989e7
geraltofrivia <devl.priyansh@gmail.com>
2015-03-29 16:25:47 +0530

    v0.15 User Registration
    Made the code such that it takes inputs from a form and registers the users.
    Changes made
    	- HTML Escaping
    	- Added three attributes to userDB
    	- Added a registration function which checks if the userID is unique or not
    	- Added a way to see if passwords match or not
    	- Added basic error message sending based on registration faults
    	- Added way of basic redirection
    
    Todo
    	- Integrate registration system with Frontend Page
    	- Figure out how to render css and js files alongwith web pages

--- main.py
+++ main.py
@@ -23,79 +23,75 @@ class Handler(webapp2.RequestHandler):
 #Setup Users DB. and its methods acting as wrappers
 class Users(ndb.Model):
 	userid = ndb.StringProperty()
-	#username = ndb.StringProperty()
-	#password = ndb.StringProperty()
-	#visits = ndb.IntegerProperty()
+	username = ndb.StringProperty()
+	password = ndb.StringProperty()
+	visits = ndb.IntegerProperty()
 
 	@classmethod
-	def getUsers(self):
-		query = self.query()
-		print query
-		return query
+	def getUserIDs(self):
+		users = []
+		query = self.query(projection=[Users.userid])
+		for user in query: users.append(str(user.userid))
+		print users
+		return users
+
+	@classmethod
+	def register(self,_userid,_username,_password):
+		print "Registering %s" %_username
+		if not _userid in self.getUserIDs():
+			user = Users(userid = _userid, username = _username, password = _password)
+			user.put()
+			return (0,'Success')
+		else:
+			return (-1,'Userid already exists. Please select a different one')			
+
+class Registration(Handler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+		self.render("registration.html", userid = "Enter a unique userid", username = "Enter your name")
+
+	def post(self):
+		register_status = (0,'Not begun yet') #If status = 0, so far success. If it goes -1, something's wrong
+		self.response.headers['Content-Type'] = 'text/html'
+		_userid = self.request.get('userid')
+		_username = self.request.get('username')
+		_password = self.request.get('password')
+		_c_password = self.request.get('c_password')
+
+		_username = str(cgi.escape(_username,quote="True"))
+		_userid = str(cgi.escape(_userid,quote="True"))
+
+		#Encrypt the pwd
+
+		#Fix the value of all inputs
+		if _password != _c_password:
+			register_status = (-1,'The passwords do not match!')
+
+		#Attempt to register. Return value corresponding to success or failure
+		if register_status[0] == 0:
+			register_status = Users.register(_userid,_username,_password)
+			print register_status
+		
+
+		if register_status[0] != 0:
+			self.render("registration.html", error = register_status[1], userid = _userid, username = _username)
+		else:
+			self.redirect("/getusers/")		#Change to homepage.
+
 
 class MainPage(Handler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		self.render("login.html")
-
-	def post(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		_userid = self.request.get('username')
-		print "About to add '", _userid, "' to the database"
-		entry = Users(userid = _userid)
-		entry.put()
+		self.write("Welcome!")
 
 class PrintUsers(Handler):
 	def get(self):
-		queries = Users.getUsers()
+		queries = Users.getUserIDs()
 		for query in queries:
-			self.write("<p>%s</p>" % query.userid)
-
-
-"""#Basic
-class MainPage(Handler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		visits = self.request.cookies.get('visits','0')
-		userid = self.request.cookies.get('userid','guest')
-		if visits.isdigit():
-			visits = int(visits) + 1
-		else:
-			visits = 0
-		self.render("login.html", visits = visits)
-		self.response.headers.add_header('Set-cookie', 'visits = %s' % visits)
-		self.response.headers.add_header('Set-cookie', 'userid = %s' % str(userid))
-
-	def post(self):
-
-		#Fetch the username, from the form and from the cookie.
-		self.response.headers['Content-Type'] = 'text/html'
-		userid_from_cookies = self.request.cookies.get('userid','guest')
-		userid_from_form = self.request.get('username')
-		visits = self.request.cookies.get('visits','0')
-
-		#Print out the number of visits
-		if visits.isdigit():
-			visits = int(visits) + 1
-		else:
-			visits = 0
-
-		#If the names don't match, then simply welcome the new user differently.
-		if userid_from_form != userid_from_cookies:
-			self.write("Welcome Mr. %s" % userid_from_form)
-			visits = 0
-		
-		#Set cookies
-		self.response.headers.add_header('Set-cookie', 'visits = %s' % visits)
-		self.response.headers.add_header('Set-cookie', 'userid = %s' % str(userid_from_form))
-
-		#Render the form
-		self.render("welcome.html", visits = visits)	
-"""
-
+			self.write("<p>%s</p>" % query)
 
 application = webapp2.WSGIApplication([
 									('/',MainPage),
+									('/registration',Registration),
 									('/getusers/',PrintUsers)
 									], debug=True)
 
